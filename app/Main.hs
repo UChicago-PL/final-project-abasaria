7b42e9457101d471461d086329f99305d8d28888
Aadam B <abasaria@uchicago.edu>
2026-02-25 19:18:55 -0600

    working through GUI updates for medium goals

--- app/Main.hs
+++ app/Main.hs
@@ -1,36 +1,38 @@
 module Main where
 
+import Graphics.Gloss.Interface.IO.Game
+import Game.Types
 import Game.Board
 import qualified Game.GUI as GUI
-import Game.Types
-import Graphics.Gloss.Interface.IO.Game
+
+-- | Window settings
+window :: Display
+window = InWindow "Minesweeper" (600, 600) (100, 100)
+
+backgroundColor :: Color
+backgroundColor = greyN 0.7
+
+fps :: Int
+fps = 60
+
+-- | Game parameters
+rows, cols, mines :: Int
+rows = 10
+cols = 10
+mines = 10
 
 main :: IO ()
 main = do
-  -- Board parameters
-  let rows = 10
-      cols = 10
-      mines = 10
+    -- 1. Generate initial board with mines
+    b <- initBoard rows cols mines
+    let initialState = GameState b Playing
 
-  -- Initialize board
-  b <- initBoard rows cols mines
-  let initialState = GameState b Playing
-
-  -- Window and background
-  let window :: Display
-      window = InWindow "Minesweeper" (600, 600) (100, 100)
-
-      backgroundColor :: Color
-      backgroundColor = greyN 0.7
-
-      fps = 30
-
-  -- Run Gloss GUI
-  playIO
-    window
-    backgroundColor
-    fps
-    initialState
-    GUI.draw -- GameState -> IO Picture
-    GUI.handleMouse -- Event -> GameState -> IO GameState
-    GUI.update -- Float -> GameState -> IO GameState
+    -- 2. Start Gloss interactive game
+    playIO
+      window
+      backgroundColor
+      fps
+      initialState
+      GUI.draw         -- GameState -> IO Picture
+      GUI.handleMouse  -- Event -> GameState -> IO GameState
+      GUI.update       -- Float -> GameState -> IO GameState
--- src/Game/GUI.hs
+++ src/Game/GUI.hs
@@ -2,71 +2,70 @@ module Game.GUI
   ( draw
   , handleMouse
   , update
+  , cellSize
   ) where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 import Game.Types
-import Game.Board
 import Game.Logic
 
--- GUI constants
+-- | Size of each cell in pixels
 cellSize :: Float
 cellSize = 40
 
-window :: Display
-window = InWindow "Minesweeper" (600, 600) (100, 100)
-
-backgroundColor :: Color
-backgroundColor = greyN 0.7
-
 -- | Convert board coordinates to screen coordinates
 toScreen :: Pos -> (Float, Float)
-toScreen (r, c) = (fromIntegral c * cellSize - 200, 200 - fromIntegral r * cellSize)
+toScreen (r, c) =
+  (fromIntegral c * cellSize - fromIntegral cols * cellSize / 2 + cellSize/2,
+   fromIntegral (rows - r - 1) * cellSize - fromIntegral rows * cellSize / 2 + cellSize/2)
+  where
+    rows = 10  -- default board size; adjust if making dynamic
+    cols = 10
+
+-- | Draw a single cell
+drawCell :: Cell -> Picture
+drawCell c
+  | flagged c = color red (rectangleSolid cellSize cellSize)
+  | not (revealed c) = color (greyN 0.5) (rectangleSolid cellSize cellSize)
+  | hasMine c = color black (rectangleSolid cellSize cellSize)
+  | adjMines c == 0 = color white (rectangleSolid cellSize cellSize)
+  | otherwise = Pictures
+      [ color white (rectangleSolid cellSize cellSize)
+      , color black (translate (-cellSize/4) (-cellSize/4)
+                  (Scale 0.2 0.2 (Text (show (adjMines c)))))
+      ]
 
 -- | Draw the entire board
 draw :: GameState -> IO Picture
-draw (GameState b _) = return $ Pictures [ drawCellAt r c (b !! r !! c)
-                                         | r <- [0..length b - 1]
-                                         , c <- [0..length (head b) - 1] ]
-
--- | Draw a single cell at a given board position
-drawCellAt :: Int -> Int -> Cell -> Picture
-drawCellAt r c cell =
-  translate x y $ drawCell cell
-  where
-    (x, y) = toScreen (r, c)
-
-drawCell :: Cell -> Picture
-drawCell c
-  | flagged c        = color red (rectangleSolid cellSize cellSize)
-  | not (revealed c) = color (greyN 0.5) (rectangleSolid cellSize cellSize)
-  | hasMine c        = color black (rectangleSolid cellSize cellSize)
-  | adjMines c == 0  = color white (rectangleSolid cellSize cellSize)
-  | otherwise        =
-      Pictures
-        [ color white (rectangleSolid cellSize cellSize)
-        , color black (translate (-cellSize/4) (-cellSize/4)
-            (Scale 0.2 0.2 (Text (show (adjMines c)))))
-        ]
+draw gs = return $ Pictures
+  [ translate x y (Pictures [drawCell cell, drawBorder])
+  | (r, row) <- zip [0..] (board gs)
+  , (c, cell) <- zip [0..] row
+  , let (x, y) = toScreen (r, c)
+  , let drawBorder = color black (rectangleWire cellSize cellSize)
+  ]
 
 -- | Handle mouse clicks
 handleMouse :: Event -> GameState -> IO GameState
-handleMouse (EventKey (MouseButton LeftButton) Up _ (mx, my)) gs = do
-  let pos = toBoard (mx, my)
-  return $ revealCell pos gs
-handleMouse (EventKey (MouseButton RightButton) Up _ (mx, my)) gs = do
-  let pos = toBoard (mx, my)
-  return $ flagCell pos gs
+handleMouse (EventKey (MouseButton LeftButton) Down _ (mx, my)) gs =
+  let pos = screenToPos (mx, my)
+  in return $ revealCell pos gs
+handleMouse (EventKey (MouseButton RightButton) Down _ (mx, my)) gs =
+  let pos = screenToPos (mx, my)
+  in return $ flagCell pos gs
 handleMouse _ gs = return gs
 
 -- | Convert screen coordinates back to board coordinates
-toBoard :: (Float, Float) -> Pos
-toBoard (x, y) = (row, col)
+screenToPos :: (Float, Float) -> Pos
+screenToPos (x, y) =
+  (rows - 1 - r, c)
   where
-    col = floor ((x + 200) / cellSize)
-    row = floor ((200 - y) / cellSize)
+    r = floor ((y + fromIntegral rows * cellSize / 2) / cellSize)
+    c = floor ((x + fromIntegral cols * cellSize / 2) / cellSize)
+    rows = 10
+    cols = 10
 
--- | Update function (no-op for now)
+-- | Update function (no time-based updates needed)
 update :: Float -> GameState -> IO GameState
 update _ gs = return gs
